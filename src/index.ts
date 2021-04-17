8f0f5ec398f3b6caae15d0f9bed78172bb230f91
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2021-04-17 03:47:22 -0700

    build: update configurations and dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,194 +1,90 @@
-import { PluginObj, PluginPass } from '@babel/core';
-import { builtinModules } from 'module';
-import { determineModuleTypes } from 'webpack-node-module-types/sync';
+import { name as pkgName } from '../package.json';
+import { NodePath, PluginObj, PluginPass } from '@babel/core';
+import debugFactory from 'debug';
 
 import * as util from '@babel/types';
 
-export type Options = {
-  opts: {
-    test?: string[];
-    exclude?: string[];
-    transformBuiltins?: boolean;
-    silent?: boolean;
-    verbose?: boolean;
-  };
-};
+const debug = debugFactory(`${pkgName}:index`);
+let globalScope: NodePath['scope'];
 
-type State = PluginPass & Options;
+const updateExportRefs = (
+  idPath: NodePath<util.Identifier>,
+  mode: 'named' | 'default'
+) => {
+  const exportedName = idPath?.node?.name;
+  if (!exportedName)
+    throw new Error(`could not retrieve ${mode} export's name from identifier`);
 
-const cache: {
-  builtins: RegExp[];
-  cjsNodeModules: RegExp[];
-} = {
-  builtins: [],
-  cjsNodeModules: []
-};
+  globalScope = globalScope || idPath.findParent((node) => node.isProgram())?.scope;
+  if (!globalScope) throw new Error('could not find program global scope');
 
-const _metadata: {
-  [path: string]: {
-    total: number;
-    transformed: string[];
-    iTests: RegExp[];
-    eTests: RegExp[];
-  };
-} = {};
+  const refPaths = globalScope.getBinding(exportedName)?.referencePaths;
+  debug(
+    `updating ${refPaths?.length || 0} references to ${mode} export "${exportedName}"`
+  );
 
-const stringToRegex = (str: string | RegExp, openEnded: boolean) => {
-  return str instanceof RegExp
-    ? str
-    : new RegExp(
-        str.startsWith('/') && str.endsWith('/')
-          ? str.slice(1, -1)
-          : // ? https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions#Escaping
-            `^${str.replace(/[.*+\-?^${}()|[\]\\]/g, '\\$&')}${
-              openEnded ? '([/?#].+)?' : ''
-            }$`,
-        'i'
-      );
-};
-
-const strToRegex = (str: string | RegExp) => stringToRegex(str, false);
-const strToOpenEndedRegex = (str: string | RegExp) => stringToRegex(str, true);
-
-const getDefaultInclusionTests = () => {
-  return (cache.cjsNodeModules = cache.cjsNodeModules.length
-    ? cache.cjsNodeModules
-    : [
-        ...determineModuleTypes().cjs.map(strToOpenEndedRegex),
-        /^(\.(\.)?\/)+(.+)\.json$/
-      ]);
-};
-
-const getBuiltinInclusionTests = () => {
-  return (cache.builtins = cache.builtins.length
-    ? cache.builtins
-    : builtinModules.map(strToOpenEndedRegex));
-};
-
-/**
- * Return `source` as an alphanumeric string with underscores replacing
- * non-alphanumeric characters
- */
-const makeSpecifierFrom = (source: string) =>
-  '_$' + source.replace(/[^a-z0-9]/gi, '_');
-
-const isCjs = (src: string, state: State) => {
-  const { iTests, eTests } = getMetadata(state);
-  return iTests.some((r) => r.test(src)) && eTests.every((r) => !r.test(src));
-};
-
-const getMetadata = (state: State) => {
-  const key = state.filename || '<no path>';
-
-  return (_metadata[key] = _metadata[key] || {
-    total: 0,
-    transformed: [],
-    iTests: [
-      ...(state.opts.transformBuiltins !== false
-        ? getBuiltinInclusionTests()
-        : []),
-      ...(state.opts.test?.map(strToRegex) || getDefaultInclusionTests())
-    ],
-    eTests: state.opts.exclude?.map(strToRegex) || []
+  refPaths?.forEach((refPath) => {
+    if (refPath.isIdentifier()) {
+      // TODO:
+      refPath.node.name = `CHANGED_${
+        mode == 'default' ? 'TO_DEFAULT' : refPath.node.name
+      }`;
+    } else debug('(non-identifier reference path skipped)');
   });
 };
 
-export default function (): PluginObj<State> {
+/**
+ * 1. Get the name from the identifier of the default export if it is a function
+ *    or class declaration
+ * 2. Gather names from the identifiers of non-default exports:
+ *      - If there is a non-variable declaration, get the name from the function
+ *        or class
+ *      - If there is a variable declaration, gather n
+ */
+export default function (): PluginObj<PluginPass> {
   return {
-    name: 'babel-plugin-explicit-exports-references',
+    name: 'explicit-exports-references',
     visitor: {
-      Program: {
-        enter(_, state) {
-          // ? Create it if it doesn't exist already, or do it later
-          getMetadata(state);
-        },
-        exit(_, state) {
-          if (state.opts.silent === false) {
-            const { total, transformed } = getMetadata(state);
+      ExportDefaultDeclaration(exportPath) {
+        const declaration = exportPath.get('declaration');
+        const typeStr = `declaration type "${declaration.type}"`;
+        debug(`saw ${typeStr}`);
 
-            const details =
-              `${transformed.length}/${total}` +
-              (state.opts.verbose && transformed.length
-                ? ` [${transformed.join(', ')}]`
-                : '');
-
-            if (state.opts.verbose || transformed.length)
-              // eslint-disable-next-line no-console
-              console.log(
-                `target: ${state.filename}\nimports transformed: ${details}\n---`
-              );
-          }
+        if (declaration.isFunctionDeclaration() || declaration.isClassDeclaration()) {
+          const id = declaration.get('id') as NodePath<util.Identifier>;
+          if (id?.node?.name) updateExportRefs(id, 'default');
+          else debug('default declaration is anonymous, ignoring');
         }
       },
-      ImportDeclaration(path, state) {
-        const source = path.node.source.value;
+      ExportNamedDeclaration(exportPath) {
+        const declaration = exportPath.get('declaration');
+        const specifiers = exportPath.get('specifiers');
 
-        getMetadata(state).total++;
+        if (!declaration.node && !specifiers.length) {
+          debug('ignored empty named export declaration');
+          return;
+        } else if (declaration.node && specifiers.length) {
+          throw new Error(
+            'named exports cannot have both a) a declaration and b) specifiers'
+          );
+        } else debug('(ignored)');
 
-        if (!isCjs(source, state)) return;
+        const typeStr = `declaration type "${declaration.type}"`;
+        debug(`saw ${typeStr}`);
 
-        const specifiers = {
-          explicitDefault: '',
-          implicitDefault: '',
-          namespace: '',
-          named: [] as { actual: string; alias: string | null }[]
-        };
+        if (declaration.node) {
+          if (declaration.isFunctionDeclaration() || declaration.isClassDeclaration()) {
+            updateExportRefs(declaration.get('id') as NodePath<util.Identifier>, 'named');
+          } else if (declaration.isVariableDeclaration()) {
+            declaration.get('declarations').forEach((declarator) => {
+              const id = declarator.get('id');
+              if (id.isIdentifier()) updateExportRefs(id, 'named');
+              else debug('(non-identifier id skipped)');
+            });
+          } else debug('(ignored)');
+        }
 
-        path.node.specifiers.forEach((node) => {
-          if (util.isImportSpecifier(node)) {
-            const name = util.isStringLiteral(node.imported)
-              ? node.imported.value
-              : node.imported.name;
-
-            if (name == 'default') specifiers.implicitDefault = node.local.name;
-
-            if (name != 'default' || specifiers.explicitDefault) {
-              specifiers.named.push({
-                actual: name,
-                alias: name != node.local.name ? node.local.name : null
-              });
-            }
-          } else if (util.isImportDefaultSpecifier(node))
-            specifiers.explicitDefault = node.local.name;
-          else if (util.isImportNamespaceSpecifier(node))
-            specifiers.namespace = node.local.name;
-        });
-
-        if (!specifiers.named.length) return;
-
-        const newDefaultSpecifier = util.identifier(
-          specifiers.explicitDefault ||
-            specifiers.implicitDefault ||
-            makeSpecifierFrom(source)
-        );
-
-        // ? Transform the import into a default CJS import
-        path.node.specifiers = [
-          util.importDefaultSpecifier(newDefaultSpecifier)
-        ];
-
-        // ? Insert a constant destructing assignment after
-
-        const declaration = util.variableDeclaration('const', [
-          util.variableDeclarator(
-            util.objectPattern(
-              specifiers.named.map((s) =>
-                util.objectProperty(
-                  util.identifier(s.actual),
-                  util.identifier(s.alias ?? s.actual),
-                  false,
-                  !s.alias
-                )
-              )
-            ),
-            newDefaultSpecifier
-          )
-        ]);
-
-        path.insertAfter(declaration);
-
-        getMetadata(state).transformed.push(source);
+        if (specifiers.length) {
+        }
       }
-    }
-  };
 }
