62b6623b4030bce0ce2fac9eb07230dcd01a1a92
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2023-01-17 12:13:14 -0800

    ci: update tooling

---
+++ .ncurc.js
@@ -0,0 +1,9 @@
+// * https://www.npmjs.com/package/npm-check-updates#configuration-files
+'use strict';
+
+module.exports = {
+  reject: [
+    // ? Pin the CJS version
+    'execa'
+  ]
+};
--- babel.config.js
+++ babel.config.js
@@ -5,34 +5,33 @@
 const NODE_OLDEST_LTS = '10.13.0';
 
 module.exports = {
-    parserOpts: { strictMode: true },
-    plugins: [
-        '@babel/plugin-proposal-export-default-from',
-        '@babel/plugin-proposal-function-bind',
-        '@babel/plugin-transform-typescript',
-    ],
-    env: {
-        // * Used by Jest and `npm test`
-        test: {
-            sourceMaps: 'both',
-            presets: [
-                ['@babel/preset-env', { targets: { node: true }}],
-                ['@babel/preset-typescript', { allowDeclareFields: true }],
-                // ? We don't care about minification
-            ]
-        },
-        // * Used by `npm run build`
-        production: {
-            presets: [
-                ['@babel/preset-env', {
-                    // ? https://github.com/babel/babel-loader/issues/521#issuecomment-441466991
-                    //modules: false,
-                    // ? https://nodejs.org/en/about/releases
-                    targets: { node: NODE_OLDEST_LTS }
-                }],
-                ['@babel/preset-typescript', { allowDeclareFields: true }],
-                // ? Webpack will handle minification
-            ]
-        },
+  parserOpts: { strictMode: true },
+  plugins: ['@babel/plugin-proposal-export-default-from'],
+  env: {
+    // * Used by Jest and `npm test`
+    test: {
+      sourceMaps: 'both',
+      presets: [
+        ['@babel/preset-env', { targets: { node: true } }],
+        ['@babel/preset-typescript', { allowDeclareFields: true }]
+        // ? We don't care about minification
+      ]
+    },
+    // * Used by `npm run build`
+    production: {
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            // ? https://github.com/babel/babel-loader/issues/521#issuecomment-441466991
+            //modules: false,
+            // ? https://nodejs.org/en/about/releases
+            targets: { node: NODE_OLDEST_LTS }
+          }
+        ],
+        ['@babel/preset-typescript', { allowDeclareFields: true }]
+        // ? Webpack will handle minification
+      ]
     }
+  }
 };
